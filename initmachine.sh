3153df5de6fc65d223cb65f0924674b49d3191cb
Kenneth Kasajian <ken@kasajian.com>
2015-05-16 16:56:28 -0700

    .

--- initmachine.sh
+++ initmachine.sh
@@ -3,8 +3,12 @@
 # ~/p is installed from here:
 #   https://kasajian.visualstudio.com/DefaultCollection/_git/p
 
+
+# Haven't tested with non-link, but actual, ~/.vim dir existing
+
 chmod +x ~/dotfiles/makesymlinks.sh
 ~/dotfiles/makesymlinks.sh
+
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
         git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
