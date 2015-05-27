a27efddde3ec991446faaceda3fb018221eb43db
Kenneth Kasajian <ken@kasajian.com>
2015-05-27 10:20:04 -0700

    .

--- initmachine.sh
+++ initmachine.sh
@@ -122,3 +122,19 @@ if [[ $os == 'Darwin' ]]; then
     #done
     #
 fi
+
+# Git config
+git config --global user.name "Your Name Comes Here"
+git config --global user.email youremail@address
+git config --global core.editor 'gvim'
+git config --global diff.tool winmerge
+git config --global difftool.prompt false
+git config --global difftool.winmerge.cmd "WinMergeU.exe -r -u -dl Local -dr Remote \"$LOCAL\" \"$REMOTE\""
+git config --global merge.tool kdiff3
+git config --global mergetool.kdiff3.cmd "kdiff3 $BASE $LOCAL $REMOTE -o $MERGED"
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global core.autocrlf false
+git config --global push.default simple
+                   
