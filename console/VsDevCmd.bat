dee650f6a040884970c16c1da033ec224c6ece90
kasajian <2384797+kasajian@users.noreply.github.com>
2022-10-28 14:11:26 -0700

    .

---
+++ console/VsDevCmd.bat
@@ -0,0 +1,3 @@
+@if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" & goto :eof
+@if exist "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" & goto :eof
+
