fb7877a95a330d4ff858da5a6e7364a3ee344e4f
dev <dev@dev>
2022-11-13 00:47:22 -0800

    bash

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,6 +1,9 @@
 mkdir -p ~/.vim/pack/tpope/start
 pushd ~/.vim/pack/tpope/start
+export GIT_SSL_NO_VERIFY=1
 git clone https://tpope.io/vim/sensible.git
+cd sensible
+git pull
 popd
 
-ln -s /Users/Kenny/dotfiles/.vimrc /Users/Kenny/.vimrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
